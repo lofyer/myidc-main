32356582339ffae23279769f25a24835a7a21c3b
lofyer <lofyer@gmail.com>
2013-11-25 23:22:06 +0800

    Base OS: add test scripts

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -en '\E[37;32m'"\033[1mMyIDC - Quick Start Scripts\n\033[0m"
+echo -e '**************Base System**************'
+echo -e '1) Start Base Cluster System(using kvm)'
+echo -e '2) Start Base Cluster System(using xen)'
+echo -e '*************Control Panel*************'
+echo -e '3) Start Control Panel on http://localhost:80'
+echo -e '****************VM Cloud***************'
+echo -e '4) Start Nested Cloud Platform'
